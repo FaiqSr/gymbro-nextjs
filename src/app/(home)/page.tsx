e56ebff4482700bb7be967a9108ee6e486391c40
ilhambonardoo <ilhambonardo123@gmail.com>
2025-07-06 04:25:28 +0700

    Menambahkan tampian jadwal upper lower di date/home-date

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
+import Date from "@/components/home/Date";
 import React from "react";
 
 const page = () => {
-  return <div className="w-full">page</div>;
+  return (
+    <div className="w-full">
+      <Date />
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/home/Date.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const Date = () => {
+  const [data, setData] = useState("");
+
+  console.log(data);
+  useEffect(() => {
+    console.log("kocak");
+  }, [data]);
+
+  return (
+    <>
+      <header className="border">
+        <div className="container">
+          <h1>JADWAL FOR UPPER and LOWER</h1>
+          <data className="flex flex-wrap">
+            <div className="">
+              <h2>SENIN</h2>
+              <li>Bench Press</li>
+              <li>Lat Pulldown</li>
+              <li>Incline Press</li>
+              <li>Cable Rowing</li>
+            </div>
+            <div className="">
+              <h2>SELASA</h2>
+              <li>Squat</li>
+              <li>Bulgarian Squat</li>
+              <li>RDL</li>
+              <li>Abs</li>
+            </div>
+            <div className="">
+              <h2>RABU</h2>
+              <li>REST DAY</li>
+            </div>
+          </data>
+        </div>
+      </header>
+      <button onClick={() => setData("kocaksa")}>test</button>
+    </>
+  );
+};
+
+export default Date;
