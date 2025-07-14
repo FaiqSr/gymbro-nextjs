c6535c16a3f3fe6a080b9eb3669a96b137cd8fae
ilhambonardoo <ilhambonardo123@gmail.com>
2025-07-14 15:57:02 +0700

    add ring-2 dan mengubah false true button

--- src/components/latihan/kalkulator/KalkulatorContainer.tsx
+++ src/components/latihan/kalkulator/KalkulatorContainer.tsx
@@ -23,25 +23,25 @@ const KalkulatorContainer = () => {
         <div className="flex flex-col w-full max-w-[970px] lg:ml-20 xl:ml-72 h-auto min-h-[500px] md:min-h-[600px] lg:h-[740px] bg-black/70 rounded-xl lg:rounded-3xl mx-auto my-6 md:my-8 lg:my-15 px-4 md:px-6 lg:px-8 pt-4 pb-6 md:pt-6">
           <div className="flex flex-col md:flex-row justify-center items-center my-4 md:my-5 gap-2 md:gap-4">
             <button
-              onClick={() => setKalkulator(false)}
+              onClick={() => setKalkulator(true)}
               className={`w-full md:w-[300px] lg:w-[460px] h-12 md:h-[48px] bg-black/30 text-center rounded-3xl transition-all duration-200 ease-in-out hover:bg-abutua/90 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 focus:bg-abutua active:bg-abutua cursor-pointer ${
-                !kalkulator ? "bg-abutua/90" : ""
+                !kalkulator ? "bg-black/30 " : " ring-2"
               }`}
             >
               Kalkulator BMI
             </button>
             <button
-              onClick={() => setKalkulator(true)}
+              onClick={() => setKalkulator(false)}
               className={`w-full md:w-[300px] lg:w-[460px] h-12 md:h-[48px] bg-black/30 text-center rounded-3xl transition-all duration-200 ease-in-out hover:bg-abutua/90 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 focus:bg-abutua active:bg-abutua cursor-pointer ${
-                kalkulator ? "bg-abutua/90" : ""
+                kalkulator ? "bg-black/30" : "ring-2"
               }`}
             >
               Kalkulator IDAI
             </button>
           </div>
           <div className="flex-1">
-            <KalkulatorPageBMI className={`${kalkulator ? "hidden" : "block"}`} />
-            <KalkulatorPageIDAI className={`${kalkulator ? "block" : "hidden"}`} />
+            <KalkulatorPageBMI className={`${kalkulator ? "block" : "hidden"}`} />
+            <KalkulatorPageIDAI className={`${kalkulator ? "hidden" : "block"}`} />
           </div>
         </div>
       </section>
