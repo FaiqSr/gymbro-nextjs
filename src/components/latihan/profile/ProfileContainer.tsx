58f177b1162b7a7b6aaeef883ff9a76ba7474036
ilhambonardoo <ilhambonardo123@gmail.com>
2025-07-16 23:44:28 +0700

    mengubah sub judul

--- src/components/latihan/profile/ProfileContainer.tsx
+++ src/components/latihan/profile/ProfileContainer.tsx
@@ -16,7 +16,7 @@ const ProfileContainer = () => {
       <section className={`w-full h-full dark:bg-gradient-to-br dark:from-black dark:via-red-950 dark:to-black ${poppins.className}`}>
         <div className="my-6 md:my-8 lg:my-12 mx-4 lg:mx-15 lg:ml-20 xl:ml-80">
           <h1 className="text-2xl md:text-3xl lg:text-[32px] text-red-600 font-semibold">Profil Akun</h1>
-          <p className="text-sm md:text-base lg:text-[20px] font-semibold text-abu mt-1 md:mt-2">Perhitungan perkiraan badan ideal dan tinggi badan anak</p>
+          <p className="text-sm md:text-base lg:text-[20px] font-semibold text-abu mt-1 md:mt-2">Lihat detail informasi akun Anda disini </p>
         </div>
 
         {isEditing ? <ProfileEditPage onCancel={() => setIsEditing(false)} /> : <ProfilePage onEdit={() => setIsEditing(true)} />}
