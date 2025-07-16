acbb85048a818c4024550c933283bac2479260ea
ilhambonardoo <ilhambonardo123@gmail.com>
2025-07-16 23:38:28 +0700

    mengubah tampilan log out

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -36,62 +36,35 @@ const SideBar = () => {
       <section className={`w-full`}>
         <nav className="">
           <ul className={`flex flex-col gap-5 font-medium text-lg dark:text-white ${poppins.className}`}>
-            {/* Menu Items */}
-            <li
-              className={`${getBorderColor("/latihan")}
-                pl-2 pr-10 py-3 hover:cursor-pointer 
-                transition-all duration-300 hover:bg-abu/10 hover:rounded-3xl
-                hover:shadow-md hover:dark:shadow-coklat/30`}
-            >
+            <li className={`${getBorderColor("/latihan")}pl-2 pr-10 py-3 hover:cursor-pointer transition-all duration-300 hover:bg-abu/10 hover:rounded-3xl hover:shadow-md hover:dark:shadow-coklat/30`}>
               <Link href={`/latihan`} className={`flex gap-4 items-center`}>
                 <MdFitnessCenter size={24} className="dark:text-coklat transition-colors duration-200 hover:scale-105" />
                 <span className="hover:translate-x-1 transition-transform duration-200">Latihan</span>
               </Link>
             </li>
 
-            <li
-              className={`${getBorderColor("/chatbot")}
-                pl-2 pr-10 py-3 hover:cursor-pointer
-                transition-all duration-300 hover:bg-abu/10 hover:rounded-3xl
-                hover:shadow-md hover:dark:shadow-coklat/30`}
-            >
+            <li className={`${getBorderColor("/chatbot")} pl-2 pr-10 py-3 hover:cursor-pointer transition-duration-300 hover:bg-abu/10 hover:rounded-3xl hover:shadow-md hover:dark:shadow-coklat/30`}>
               <Link href={`/chatbot`} className={`flex gap-4 items-center`}>
                 <MdChat size={24} className="dark:text-coklat transition-colors duration-200 hover:scale-105" />
                 <span className="hover:translate-x-1 transition-transform duration-200">Chatbot</span>
               </Link>
             </li>
 
-            <li
-              className={`${getBorderColor("/kalkulator")}
-                pl-2 pr-10 py-3 hover:cursor-pointer
-                transition-all duration-300 hover:bg-abu/10 hover:rounded-3xl
-                hover:shadow-md hover:dark:shadow-coklat/30`}
-            >
+            <li className={`${getBorderColor("/kalkulator")} pl-2 pr-10 py-3 hover:cursor-pointer transition-duration-300 hover:bg-abu/10 hover:rounded-3xl hover:shadow-md hover:dark:shadow-coklat/30`}>
               <Link href={`/kalkulator`} className={`flex gap-4 items-center`}>
                 <MdCalculate size={24} className="dark:text-coklat transition-colors duration-200 hover:scale-105" />
                 <span className="hover:translate-x-1 transition-transform duration-200">Kalkulator</span>
               </Link>
             </li>
 
-            {/* Profile */}
-            <li
-              className={`${borderColorprofile("/profile")} 
-                flex flex-col items-center hover:cursor-pointer
-                transition-all duration-300 hover:bg-abu/10 hover:rounded-full
-                hover:shadow-md hover:dark:shadow-coklat/30 p-2`}
-            >
+            <li className={`${borderColorprofile("/profile")} flex flex-col items-center hover:cursor-pointer transition-all duration-300 hover:bg-abu/10 hover:rounded-full hover:shadow-md hover:dark:shadow-coklat 30 p-2`}>
               <Link href={`/profile`} className="flex flex-col items-center">
                 <MdAccountCircle size={65} className="dark:text-coklat transition-colors duration-200 hover:scale-105" />
                 <span className="mt-1 hover:translate-y-1 transition-transform duration-200">Profil</span>
               </Link>
             </li>
 
-            {/* Logout */}
-            <li
-              className="mt-28 w-[135px] h-[40px] hover:cursor-pointer items-center rounded-2xl
-                transition-all duration-300 hover:bg-abu/10
-                hover:shadow-md hover:dark:shadow-coklat/30"
-            >
+            <li className={`mt-28  hover:cursor-pointer items-center rounded-2xl transition-all pl-2 pr-10 py-2 duration-300 hover:bg-abu/10 hover:shadow-md hover:dark:shadow-coklat/30`}>
               <Link href={`/login`} className={`flex gap-2 items-center pt-1 justify-center`}>
                 <AiOutlineLogout size={24} className="dark:text-coklat transition-colors duration-200 hover:scale-105" />
                 <span className="hover:translate-x-1 transition-transform duration-200">Log Out</span>
