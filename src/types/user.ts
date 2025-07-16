51c5500401fa0d3fd327534199d9c8aed3d70781
FaiqSr <faiqsr13@gmail.com>
2025-07-16 16:30:26 +0700

    add: menambahkan api untuk konfigurasi akun user

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,28 +1,47 @@
+import { Metode, Type } from "@/generated/prisma";
+
 export type UserLoginResponse = {
-    username: string,
-    token: string,
-}
+  username: string;
+  token: string;
+};
 
 export type UserLoginRequest = {
-    usernameOrEmail: string,
-    password: string
-}
+  usernameOrEmail: string;
+  password: string;
+};
 
 export type UserRegisterRequest = {
-    name: string,
-    username: string,
-    email: string,
-    password: string
-}
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+};
 
-export type  UserRegisterResponse = {
-    username: string,
-    email: string,
-}
-
+export type UserRegisterResponse = {
+  username: string;
+  email: string;
+};
 
 export type UserCurrentResponse = {
-    username: string;
-    email: string;
-    token: string | null;
-}
+  username: string;
+  email: string;
+  token: string | null;
+  foto: string | null;
+  gender: string | null;
+  type: Type | null;
+  metode: Metode | null;
+};
+
+export type UserUpdateRequest = {
+  username?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  gender?: string;
+  type?: Type;
+  metode?: Metode;
+};
+
+export type UserUpdateResponse = {
+  username: string;
+};
